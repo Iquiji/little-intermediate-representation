e4c5be607fe09c699d2a7e82d12cdcc15ee8abc9
Iquiji <yt.failerbot.3000@gmail.com>
2022-04-30 15:25:08 +0200

    added new notes how it could eventually implemented

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,27 @@ use std::{collections::HashMap, rc::Rc};
 use little_parser::{Expression, Parser, Programm};
 
 #[derive(Debug, Clone)]
-enum LinearInstruction {
+pub enum LinearInstruction {
     Assign {
-        identifier: StaticIdentifier,
+        identifier: StaticRef,
         from_reg: Register,
         scope: Scope,
     },
+    // We need to deepclone the scope when we call tho!
     Call {
         output_reg: Register,
-        function_pointer: InitializedFunctionPointer,
-        arguments: Vec<DataPointer>,
+        /// Originaly InitializedFunctionPointer but that only adds unneded complexity without gain
+        function_pointer: Register,
+        arguments: Vec<Pointer>, // TODO: cant do this! we need some intermediate building to make these! // Maybe pointer to something always list?
     },
     Lookup {
-        identifier: StaticIdentifier,
+        identifier: StaticRef,
         to_reg: Register,
         scope: Scope,
     },
     Cond {
-        condition: DataPointer,
+        /// Pointer
+        condition: Register,
         branc_if_true: Branch,
     },
     Return {
@@ -29,63 +32,70 @@ enum LinearInstruction {
     InitializeFunctionPointer {
         function: LinearBlock,
         from_scope: Scope,
+        outpu_reg: Register,
     },
 }
 
 #[derive(Debug, Clone)]
-struct LinearBlock {
+pub struct LinearBlock {
     ident: String,
     program: Vec<LinearInstruction>,
 }
 
 #[derive(Debug, Clone)]
-struct Register {
+pub struct Register {
     virtual_ident: String,
 }
 
 #[derive(Debug, Clone)]
-struct InitializedFunctionPointer {
+pub struct InitializedFunctionPointer {
     // Stored in Translator Hashmap for now
     actual_func: String,
 }
 
 #[derive(Debug, Clone)]
-struct DataPointer {}
+pub enum Pointer {
+    StaticData(StaticRef),
+    DynamicPointer,
+}
 
 #[derive(Debug, Clone)]
-enum Scope {
+pub struct StaticRef {
+    refname: String,
+    reftype: StaticData,
+}
+
+#[derive(Debug, Clone)]
+pub enum Scope {
     Global,
     Current,
     Custom(Register),
 }
 
 #[derive(Debug, Clone)]
-struct StaticIdentifier {
-    ident: String,
-}
-
-#[derive(Debug, Clone)]
-struct Branch {
+pub struct Branch {
     program: Vec<LinearInstruction>,
 }
 
 pub struct Translator {
     register_counter: usize,
+    static_data_counter: usize,
+    static_data: HashMap<String, StaticData>,
     lambda_map: HashMap<String, LinearBlock>,
-    static_data: Vec<StaticData>,
 }
 impl Translator {
     pub fn default() -> Translator {
         Translator {
             register_counter: 0,
+            static_data_counter: 0,
             lambda_map: HashMap::new(),
-            static_data: vec![],
+            static_data: HashMap::new(),
         }
     }
-    fn ast_to_intermediate_representation(&mut self, ast: Programm) -> LinearBlock {
+    pub fn ast_to_intermediate_representation(&mut self, ast: Programm) -> LinearBlock {
         unimplemented!()
     }
-    fn expr_to_instructions(&mut self, expr: Expression) -> Vec<LinearInstruction> {
+    pub fn expr_to_instructions(&mut self, expr: Expression) -> Vec<LinearInstruction> {
         let mut instr_buf = vec![];
         match expr {
             Expression::Quote(quoted) => {
@@ -93,6 +103,7 @@ impl Translator {
             }
             Expression::Lambda(formals, body) => {
                 // Assign formals to current Scope and then execute body untill last which is returned
+                // This just initializes the LambdaFunction! => Make new Block for Lambda
             }
             Expression::Cond(cases) => {
                 // Conditions and Branches if true
@@ -114,6 +125,40 @@ impl Translator {
             }
             Expression::LambdaCall(to_call, arguments) => {
                 // Call to_call (if ident -> lookup in scope,if lambda -> Direct)
+                if let Expression::Identifier(ident) =
+                    std::rc::Rc::try_unwrap(to_call.clone()).unwrap()
+                {
+                    let shared_reg = self.make_reg_name();
+                    let static_name = self.make_static_name();
+                    self.static_data
+                        .insert(static_name.clone(), StaticData::Identifier(ident.clone()));
+                    // Lookup
+                    instr_buf.push(LinearInstruction::Lookup {
+                        identifier: StaticRef {
+                            refname: static_name,
+                            reftype: StaticData::Identifier(ident),
+                        },
+                        to_reg: shared_reg.clone(),
+                        scope: Scope::Current,
+                    });
+                    // For Arguments we can add them to the scope we push into?
+                    // How difficult is it to clone?
+                    // Do we know the functions aliases?
+
+                    // Then call
+                    instr_buf.push(LinearInstruction::Call {
+                        output_reg: (),
+                        function_pointer: shared_reg.clone(),
+                        arguments: (),
+                    })
+                } else if let Expression::Lambda(formals, body) =
+                    std::rc::Rc::try_unwrap(to_call).unwrap()
+                {
+                    // Build InitializedPointer First
+                } else {
+                    // Untrue! Lambda call can return func as well
+                    unreachable!();
+                }
             }
             Expression::Atom(atom) => {
                 // Idk is this even possible
@@ -126,10 +171,28 @@ impl Translator {
         }
         instr_buf
     }
+    fn make_reg_name(&mut self) -> Register {
+        let temp = Register {
+            virtual_ident: "r".to_owned() + &self.register_counter.to_string(),
+        };
+        self.register_counter += 1;
+        temp
+    }
+    fn make_static_name(&mut self) -> String {
+        let temp = "static".to_owned() + &self.static_data_counter.to_string();
+        self.static_data_counter += 1;
+        temp
+    }
 }
 
 #[derive(Debug, Clone)]
-enum StaticData {}
+enum StaticData {
+    Bool(bool),
+    Integer(i32),
+    String(String),
+    Identifier(String),
+    List(Vec<StaticData>),
+}
 
 #[cfg(test)]
 mod tests {
