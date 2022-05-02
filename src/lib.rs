6f07178c853b3dc5401512be83eb442d33756d81
Iquiji <yt.failerbot.3000@gmail.com>
2022-05-02 19:51:12 +0200

    Added Stack Push and Pop for IR

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, rc::Rc};
+use std::collections::HashMap;
 
 use little_parser::{Expression, Parser, Programm};
 
@@ -6,6 +6,21 @@ use little_parser::{Expression, Parser, Programm};
 pub enum LinearInstruction {
     // Need new list building instructions // one for init and one for adding!
     // TODO: rethink register saving?
+    // Stack push pop for saving registers!
+    // Inneficient but i dont care!
+    PushToStack {
+        register: Register,
+    },
+    PopFromStack {
+        register: Register,
+    },
+    LinkedListInit {
+        output_reg: Register,
+    },
+    LinkedListAdd {
+        linked_list_reg: Register,
+        input_reg: Register,
+    },
     Assign {
         identifier: StaticRef,
         from_reg: Register,
@@ -16,7 +31,7 @@ pub enum LinearInstruction {
         output_reg: Register,
         /// Originaly InitializedFunctionPointer but that only adds unneded complexity without gain
         function_pointer: Register,
-        arguments: Vec<Pointer>, // TODO: cant do this! we need some intermediate building to make these! // Maybe pointer to something always list?
+        arguments: Register, // TODO: cant do this! we need some intermediate building to make these! // Maybe pointer to something always list?
     },
     Lookup {
         identifier: StaticRef,
@@ -116,20 +131,32 @@ impl Translator {
                     std::rc::Rc::<little_parser::Expression>::try_unwrap(body).unwrap(),
                 );
 
-                // instr_buf.push(LinearInstruction::Assign {
-                //     identifier: (),
-                //     from_reg: (),
-                //     scope: (),
-                // })
+                let static_ref = StaticRef {
+                    refname: self.make_static_name(),
+                    reftype: StaticData::String(global_ident.clone()),
+                };
+                self.static_data
+                    .insert(static_ref.refname.clone(), static_ref.reftype.clone());
+
+                let reg_with_data_assigned = self.make_reg_name();
+                instr_buf.push(LinearInstruction::PopFromStack {
+                    register: reg_with_data_assigned.clone(),
+                });
+
+                instr_buf.push(LinearInstruction::Assign {
+                    identifier: static_ref,
+                    from_reg: reg_with_data_assigned,
+                    scope: Scope::Current,
+                })
+                // What do we push onto the stack?! what if we define in a function? we destroy the stack balance??
             }
             Expression::Let(bindings, body) => {
                 // Assign bindings in order and then execute body untill last which is returned in a way
             }
             Expression::LambdaCall(to_call, arguments) => {
+                let to_call = std::rc::Rc::try_unwrap(to_call.clone()).unwrap();
                 // Call to_call (if ident -> lookup in scope,if lambda -> Direct)
-                if let Expression::Identifier(ident) =
-                    std::rc::Rc::try_unwrap(to_call.clone()).unwrap()
-                {
+                if let Expression::Identifier(ident) = to_call {
                     let shared_reg = self.make_reg_name();
                     let static_name = self.make_static_name();
                     self.static_data
@@ -143,22 +170,97 @@ impl Translator {
                         to_reg: shared_reg.clone(),
                         scope: Scope::Current,
                     });
+                    instr_buf.push(LinearInstruction::PushToStack {
+                        register: shared_reg.clone(),
+                    });
+                    let args_list = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::LinkedListInit {
+                        output_reg: args_list.clone(),
+                    });
+                    instr_buf.push(LinearInstruction::PushToStack {
+                        register: args_list.clone(),
+                    });
+
                     // For Arguments we can add them to the scope we push into?
                     // How difficult is it to clone?
                     // Do we know the functions aliases?
 
+                    // Converting to a linked list and a function pointer ontop of the stack
+                    instr_buf.extend_from_slice(&self.body_to_instruction_list_with_list_to_pop_from_stack_first_in_stack_is_linked_list(arguments));
+
+                    let args_list = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::PopFromStack {
+                        register: args_list.clone(),
+                    });
+                    let function_pointer = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::LinkedListInit {
+                        output_reg: function_pointer.clone(),
+                    });
+                    let output_reg = self.make_reg_name();
                     // Then call
                     instr_buf.push(LinearInstruction::Call {
-                        output_reg: (),
-                        function_pointer: shared_reg.clone(),
-                        arguments: (),
-                    })
-                } else if let Expression::Lambda(formals, body) =
-                    std::rc::Rc::try_unwrap(to_call).unwrap()
-                {
+                        output_reg: output_reg,
+                        function_pointer: function_pointer,
+                        arguments: args_list.clone(),
+                    });
+                    instr_buf.push(LinearInstruction::PushToStack {
+                        register: args_list,
+                    });
+                } else if let Expression::Lambda(formals, body) = to_call {
                     // Build InitializedPointer First
+                    instr_buf.extend_from_slice(
+                        &self.expr_to_instructions(Expression::Lambda(formals, body)),
+                    );
+
+                    instr_buf.extend_from_slice(&self.body_to_instruction_list_with_list_to_pop_from_stack_first_in_stack_is_linked_list(arguments));
+
+                    // Call func with the arg list and func pointer pushed onto the stack earlier
+                    let args_list = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::PopFromStack {
+                        register: args_list.clone(),
+                    });
+                    let function_pointer = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::LinkedListInit {
+                        output_reg: function_pointer.clone(),
+                    });
+                    let output_reg = self.make_reg_name();
+                    // Then call
+                    instr_buf.push(LinearInstruction::Call {
+                        output_reg: output_reg,
+                        function_pointer: function_pointer,
+                        arguments: args_list.clone(),
+                    });
+                    instr_buf.push(LinearInstruction::PushToStack {
+                        register: args_list,
+                    });
+                } else if let Expression::LambdaCall(formals, body) = to_call {
+                    instr_buf.extend_from_slice(
+                        &self.expr_to_instructions(Expression::LambdaCall(formals, body)),
+                    );
+
+                    instr_buf.extend_from_slice(&self.body_to_instruction_list_with_list_to_pop_from_stack_first_in_stack_is_linked_list(arguments));
+
+                    // Call func with the arg list and func pointer pushed onto the stack earlier
+                    let args_list = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::PopFromStack {
+                        register: args_list.clone(),
+                    });
+                    let function_pointer = self.make_reg_name();
+                    instr_buf.push(LinearInstruction::LinkedListInit {
+                        output_reg: function_pointer.clone(),
+                    });
+                    let output_reg = self.make_reg_name();
+                    // Then call
+                    instr_buf.push(LinearInstruction::Call {
+                        output_reg: output_reg,
+                        function_pointer: function_pointer,
+                        arguments: args_list.clone(),
+                    });
+                    instr_buf.push(LinearInstruction::PushToStack {
+                        register: args_list,
+                    });
                 } else {
-                    // Untrue! Lambda call can return func as well
+                    // Should be correct?!
                     unreachable!();
                 }
             }
@@ -184,6 +286,32 @@ impl Translator {
         let temp = "static".to_owned() + &self.static_data_counter.to_string();
         self.static_data_counter += 1;
         temp
+    }
+    fn body_to_instruction_list_with_list_to_pop_from_stack_first_in_stack_is_linked_list(
+        &mut self,
+        body: Vec<Expression>,
+    ) -> Vec<LinearInstruction> {
+        let mut instr_buf = vec![];
+        for arg in body {
+            instr_buf.extend_from_slice(&self.expr_to_instructions(arg));
+
+            let to_add_reg = self.make_reg_name();
+            instr_buf.push(LinearInstruction::PopFromStack {
+                register: to_add_reg.clone(),
+            });
+            let args_list = self.make_reg_name();
+            instr_buf.push(LinearInstruction::LinkedListInit {
+                output_reg: args_list.clone(),
+            });
+            instr_buf.push(LinearInstruction::LinkedListAdd {
+                linked_list_reg: args_list.clone(),
+                input_reg: to_add_reg.clone(),
+            });
+            instr_buf.push(LinearInstruction::PushToStack {
+                register: args_list.clone(),
+            });
+        }
+        instr_buf
     }
 }
 
